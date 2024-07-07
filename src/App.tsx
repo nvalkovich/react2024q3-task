91cc0bcb42f190e3fb80a7eb7155432ea98c457d
nvalkovich <anastasiya.valkovich@mail.ru>
2024-07-07 20:48:32 +0300

    feat: implement search value saving

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import SearchBar from './components/SearchBar/SearchBar';
 import CardsList from './components/CardsList/CardsList';
 import './App.css';
 
-
 class App extends Component {
+  constructor(props: object) {
+    super(props);
+
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(searchQuery: string) {
+    console.log(searchQuery);
+  }
+
   render() {
     return (
       <div className='app-container'>
-        <SearchBar />
+        <SearchBar onSearch={this.handleSearch}/>
         <CardsList />
       </div>
     );
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,16 +1,45 @@
-import { Component } from 'react';
+import { ChangeEvent, Component } from 'react';
 import './SearchBar.css';
 
+type SearchProps = {
+  onSearch: (query: string) => void;
+};
 
-class SearchBar extends Component {
+type SearchState = {
+  value: string,
+};
+
+const searchQueryKey = 'searchQuery';
+
+class SearchBar extends Component<SearchProps, SearchState> {
+  constructor(props: SearchProps) {
+    super(props);
+    const query = localStorage.getItem(searchQueryKey);
+    this.state = { value: query ?? '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleClick() {
+    localStorage.setItem(searchQueryKey, this.state.value);
+    this.props.onSearch(this.state.value);
+  }
+
   render() {
     return (
       <div className='search-container'>
         <input
           type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
           className="input"
         />
-        <button className="btn">Search</button>
+        <button onClick={this.handleClick} className="btn">Search</button>
       </div>
     );
   }
